8566117b64d7683c514c5dbfaaf630bc4eb3dfdc
FrancoTrivini <frandeelejalde@gmail.com>
2026-05-08 16:35:02 -0300

    Feat: crear componente about. Modificar config para evitar errores de ruteo.

---
+++ Frontend/src/app/features/about/about.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [],
+  templateUrl: './about.html',
+  styleUrl: './about.css'
+})
+export class AboutComponent {
+  equipo = [
+    { nombre: 'Anabella Lujan Medrano', rol: 'Scrum Master', inicial: 'A' },
+    { nombre: 'Claudia Del Pilar Farias', rol: 'Desarrollador', inicial: 'C' },
+    { nombre: 'Franco Agustin Trivini De Ejalde', rol: 'Desarrollador', inicial: 'F' },
+    { nombre: 'Gabriel Agustin Pavon Molina', rol: 'Desarrollador', inicial: 'G' },
+    { nombre: 'Jesica Analia Aramayo', rol: 'Desarrollador', inicial: 'J' },
+    { nombre: 'Sofia Gimena Ledesma', rol: 'Desarrollador', inicial: 'S' }    
+  ];
+}
