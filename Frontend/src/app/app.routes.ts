a7a397f62468879da4665ec653e45ef6ee6993b9
claudiafarias2022 <claudiafarias1881@gmail.com>
2026-05-27 10:47:10 -0300

    refactor: agregar titulo a las rutas

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -4,34 +4,48 @@ import { Hero } from './features/hero/hero';
 export const routes: Routes = [
   {
     path: '',
-    component: Hero
+    component: Hero,
+    pathMatch: 'full',
+    title: 'Librería Nazareth - Inicio'
   },
   {
     path: 'about',
-    loadComponent: () => import('./features/about/about').then(c => c.AboutComponent)
+    loadComponent: () => import('./features/about/about').then(c => c.AboutComponent),
+    title: 'Sobre Nosotros'
   },
   {
     path: 'login',
-    loadComponent: () => import('./features/login/login').then(c => c.Login)
+    loadComponent: () => import('./features/login/login').then(c => c.Login),
+    title: 'Acceso al Sistema'
   },
   {
     path: 'menu',
-    loadComponent: () => import('./features/menu/menu.component').then(c => c.MenuComponent)
+    loadComponent: () => import('./features/menu/menu.component').then(c => c.MenuComponent),
+    title: 'Menú de Operaciones'
   },
   {
     path: 'cargar-producto',
-    loadComponent: () => import('./features/cargar-producto/cargar-producto').then(c => c.CargarProducto)
+    loadComponent: () => import('./features/cargar-producto/cargar-producto').then(c => c.CargarProducto),
+    title: 'Registrar Producto'
   },
   {
     path: 'info',
-    loadComponent: () => import('./features/info/info').then(c => c.Info)
+    loadComponent: () => import('./features/info/info').then(c => c.Info),
+    title: 'Información del Sistema'
   },
   {
     path: 'home',
-    loadComponent: () => import('./features/home/home').then(c => c.Home)
+    loadComponent: () => import('./features/home/home').then(c => c.Home),
+    title: 'Panel Principal'
   },
   {
     path: 'stock',
-    loadComponent: () => import('./features/stock/stock.component').then(c => c.StockComponent)
+    loadComponent: () => import('./features/stock/stock.component').then(c => c.StockComponent),
+    title: 'Gestión de Inventario'
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
   },
 ];
