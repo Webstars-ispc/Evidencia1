8d176526e5c44a970f48f671c977ad7623c84e23
Jesica-A <jess.ara95@outlook.com>
2026-06-11 19:17:21 -0300

    delete: archivos spec.ts y ssr

--- Frontend/src/app/app.config.server.ts
+++
@@ -1,6 +0,0 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { appConfig } from './app.config';
-
-// SSR deshabilitado. En lugar de prerender/SSR, la app se comporta como SPA normal.
-export const config = mergeApplicationConfig(appConfig, {} as ApplicationConfig);
-
